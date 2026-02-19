24cfdc79a8b3a38994d0895a577824cfeb23c8f2
pranathi vanga <vangapranathi@gmail.com>
2026-02-19 15:43:16 +0530

    Day 1: Project setup and health endpoint

---
+++ api-shield/src/test/java/com/api_shield/api_shield/ApiShieldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_shield.api_shield;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiShieldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
