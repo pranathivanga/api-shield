43fe8d17cb0c4c7eac1dcc5c5e0c2fc046d01c43
pranathi vanga <vangapranathi@gmail.com>
2026-03-01 15:00:12 +0530

    basic rate limiting with Redis

--- api-shield/src/main/java/com/api_shield/api_shield/interceptor/RequestLoggingInterceptor.java
+++ api-shield/src/main/java/com/api_shield/api_shield/interceptor/RequestLoggingInterceptor.java
@@ -1,24 +1,46 @@
 package com.api_shield.api_shield.interceptor;
 
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerInterceptor;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import java.util.concurrent.TimeUnit;
+
 @Component
 public class RequestLoggingInterceptor implements HandlerInterceptor {
+
+    private static final int LIMIT = 5;
+    private static final int WINDOW_SECONDS = 60;
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public RequestLoggingInterceptor(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
     @Override
     public boolean preHandle(
             HttpServletRequest request,
             HttpServletResponse response,
-            Object handler) {
+            Object handler) throws Exception {
 
         String path = request.getRequestURI();
-        System.out.println("Incoming request: " + path);
+        String key = "rate_limit:" + path;
 
-        // true = allow request to continue
-        return true;
+        Long count = redisTemplate.opsForValue().increment(key);
+
+        if (count != null && count == 1) {
+            redisTemplate.expire(key, WINDOW_SECONDS, TimeUnit.SECONDS);
+        }
+
+        if (count != null && count > LIMIT) {
+            response.setStatus(429);
+            response.getWriter().write("Too many requests. Try again later.");
+            return false; // BLOCK request
+        }
+
+        return true; // ALLOW request
     }
 }
